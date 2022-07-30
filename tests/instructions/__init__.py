76a22a47d213817addb54d5fa623bf454a917645
Lukas Hahne <lukas.hahne@posteo.de>
2022-07-31 00:26:58 +0200

    experimenting with instructions package

--- tests/instructions/__init__.py
+++ tests/instructions/__init__.py
@@ -1,7 +1,20 @@
-__all__ = ["i_instruction", "r_instruction", "sb_instruction", "uj_instruction"]
+# __all__ = ["i_instruction", "r_instruction", "sb_instruction", "uj_instruction"]
 
 from tests.instructions.i_instruction import IInstruction
 from tests.instructions.interface import InstructionType
 from tests.instructions.r_instruction import RInstruction
 from tests.instructions.sb_instruction import BInstruction, SInstruction
+from tests.instructions import sb_instruction
 from tests.instructions.uj_instruction import JInstruction
+
+
+b_instructions = [   sb_instruction.BEQop,
+                    sb_instruction.BNEop,
+                    sb_instruction.BLTop,
+                    sb_instruction.BGEop,
+                    sb_instruction.BLTUop,
+                    sb_instruction.BGEUop
+                    ]
+
+s_instructions = [  sb_instruction.SWop,
+                    ]
