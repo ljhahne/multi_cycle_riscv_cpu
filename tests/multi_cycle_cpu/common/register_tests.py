aaa883cb43e5fc04645ea4d80fc04c06b74e5725
Lukas Hahne <commercial.lukas.hahne@posteo.de>
2022-07-31 23:04:14 +0200

    register content needs to be casted to uint to make it comparible to the device register

--- tests/multi_cycle_cpu/common/register_tests.py
+++ tests/multi_cycle_cpu/common/register_tests.py
@@ -1,6 +1,10 @@
+from bitstring import Bits
+
 from tests.common import check_value
 
 
 def test_register_file(datapath, registerfile, N=32):
     for i in range(N):
-        check_value(datapath.registerFile.rf[i], registerfile[i])
+        check_value(
+            datapath.registerFile.rf[i], Bits(int=registerfile[i], length=N).uint
+        )
