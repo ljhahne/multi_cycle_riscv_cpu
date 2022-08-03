25d8fe7af620fa072c5bd19f478b12a03120f626
Lukas Hahne <commercial.lukas.hahne@posteo.de>
2022-08-03 16:47:17 +0200

    added lui op

--- src/multi_cycle_cpu/controller/instruction_decoder.sv
+++ src/multi_cycle_cpu/controller/instruction_decoder.sv
@@ -2,25 +2,29 @@
 `include "riscv.vh"
 
 module instruction_decoder(input  opcodetype  op,
-                               output logic [1:0] ImmSrc);
+                               output logic [2:0] ImmSrc);
 
     // only ImmSrc of single cycle instruction decoder
     always_comb
         case(op)
             lw_op           :
-                ImmSrc = 2'b00;
+                ImmSrc = 3'b000;
             sw_op           :
-                ImmSrc = 2'b01;
+                ImmSrc = 3'b001;
             r_type_op       :
-                ImmSrc = 2'bxx;
+                ImmSrc = 3'bxxx;
             beq_op          :
-                ImmSrc = 2'b10;
+                ImmSrc = 3'b010;
             i_type_alu_op   :
-                ImmSrc = 2'b00;
+                ImmSrc = 3'b000;
             jal_op          :
-                ImmSrc = 2'b11;
+                ImmSrc = 3'b011;
+
+            lui_op            :
+                ImmSrc = 3'b100;
+
             default        :
-                ImmSrc = 2'bxx;
+                ImmSrc = 3'bxxx;
         endcase
 
     endmodule
